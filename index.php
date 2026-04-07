c489209e4964395eb605588b1600c41a9325633c
gianfrancococchieri-commits <gianfranco.cocchieri@gmail.com>
2026-04-07 18:25:37 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+// Simple PHP application for Dynatrace demo
+class Example {
+    public function run() {
+        echo "Dynatrace PHP Demo OK\n";
+    }
+}
+
+$example = new Example();
+$example->run();
